446e3b582b89ccde0414192c0282fa75ef17c9d3
Anton Mukovozov <graalogosh@gmail.com>
2017-01-18 01:49:48 +0300

    Init from start.spring.io

---
+++ src/main/java/ru/graalogosh/PrintMasterApplication.java
@@ -0,0 +1,12 @@
+package ru.graalogosh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrintMasterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrintMasterApplication.class, args);
+	}
+}
---
+++ src/test/java/ru/graalogosh/PrintMasterApplicationTests.java
@@ -0,0 +1,16 @@
+package ru.graalogosh;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PrintMasterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
