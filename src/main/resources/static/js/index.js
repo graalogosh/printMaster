bcc076e50493c73685f88ad43cefc6e8d5c3ba2c
Anton Mukovozov <graalogosh@gmail.com>
2017-01-20 17:28:44 +0300

    VueJS init
    and playing...

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -1,15 +1,25 @@
 /**
  * Created by graal on 20.01.2017.
  */
-import _ from 'lodash';
+// import Lodash from 'lodash';
+import Vue from 'vue';
 
-function component () {
-    var element = document.createElement('div');
+// function component () {
+//     // var element = document.createElement('div');
+//     //
+//     // /* lodash is required for the next line to work */
+//     // element.innerHTML = _.join(['Hello','webpack'], ' ');
+//     //
+//     // return element;
+//
+//
+// }
+//
+// document.body.appendChild(component());
 
-    /* lodash is required for the next line to work */
-    element.innerHTML = _.join(['Hello','webpack'], ' ');
-
-    return element;
-}
-
-document.body.appendChild(component());
+var app = new Vue({
+    el:'#app',
+    data:{
+        message:'Hello Vue!'
+    }
+});
