c048710320ef03dbc1fcf1d92a5aabaf4fedfaa1
Anton Mukovozov <graalogosh@gmail.com>
2017-01-20 16:35:03 +0300

    Webpack init

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Anton Mukovozov (graalogosh@gmail.com) on 20.01.2017.
+ **/
+module.exports = {
+    entry: './src/main/resources/static/js/index.js',
+    output: {
+        filename: 'bundle.js',
+        path: './src/main/resources/static/app'
+    }
+}
